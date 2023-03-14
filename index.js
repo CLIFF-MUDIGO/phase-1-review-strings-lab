8b418ff80eff71752baa57a1e4bcd90d1143c024
cliff <cliffmudigo5@gmail.com>
2023-03-14 14:48:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser='cliff';
+const welcomeMessage="Welcome to Flatbook, ${cliff}!";
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, ${CLIFF}!";
+let shortGreeting="Welcome, !";
